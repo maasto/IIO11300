d252a4a3ad6c165571b2a10a89f0b51e4656ed02
maasto <G7693@student.jamk.fi>
2016-01-14 10:48:03 +0200

    added business logic

--- IIO11300Vktehtavat/Tehtava1/MainWindow.xaml.cs
+++ IIO11300Vktehtavat/Tehtava1/MainWindow.xaml.cs
@@ -1,8 +1,8 @@
 ﻿/*
 * Copyright (C) JAMK/IT/Esa Salmikangas
 * This file is part of the IIO11300 course project.
-* Created: 12.1.2016 Modified: 13.1.2016
-* Authors: Tero ,Esa Salmikangas
+* Created: 12.1.2016 Modified: 14.1.2016
+* Authors: Jere Halme ,Esa Salmikangas
 */
 using System;
 using System.Collections.Generic;
@@ -21,23 +21,31 @@ using System.Windows.Shapes;
 
 namespace Tehtava1
 {
-  /// <summary>
-  /// Interaction logic for MainWindow.xaml
-  /// </summary>
-  public partial class MainWindow : Window
-  {
-    public MainWindow()
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
     {
-      InitializeComponent();
-    }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
 
         private void btnCalculate_Click(object sender, RoutedEventArgs e)
         {
             //TODO
             try
             {
-                double result;
-                result = BusinessLogicWindow.CalculatePerimeter(1, 1);
+                double windowArea;
+                double windowPaneCircumference;
+                double windowPaneArea;
+                windowArea = BusinessLogicWindow.CalculateWindowArea(txtWidht.Text, txtHeight.Text);
+                windowPaneCircumference = BusinessLogicWindow.CalculateWindowPaneCircumference(txtWidht.Text, txtHeight.Text, txtThickness.Text);
+                windowPaneArea = BusinessLogicWindow.CalculateWindowPaneArea(txtWidht.Text, txtHeight.Text, txtThickness.Text);
+                tbWindowAreaResult.Text = Convert.ToString(windowArea);
+                tbWindowPaneCircumferenceResult.Text = Convert.ToString(windowPaneCircumference);
+                tbWindowPaneAreaResult.Text = Convert.ToString(windowPaneArea);
+
             }
             catch (Exception ex)
             {
@@ -49,20 +57,30 @@ namespace Tehtava1
             }
         }
 
-    private void btnClose_Click(object sender, RoutedEventArgs e)
-    {
-      Application.Current.Shutdown();
-    }
-  }
-
-  public class BusinessLogicWindow
-    {
-    /// <summary>
-    /// CalculatePerimeter calculates the perimeter of a window
-    /// </summary>
-    public static double CalculatePerimeter(double widht, double height)
+        private void btnClose_Click(object sender, RoutedEventArgs e)
         {
-            throw new System.NotImplementedException();
+            Application.Current.Shutdown();
+        }
+
+    }
+
+    public class BusinessLogicWindow
+    {
+        /// <summary>
+        /// CalculatePerimeter calculates the perimeter of a window
+        /// </summary>
+        public static double CalculateWindowArea(string widht, string height)
+        {
+
+            return (Convert.ToDouble(widht) * Convert.ToDouble(height));
+        }
+        public static double CalculateWindowPaneCircumference(string widht, string height, string thickness)
+        {
+            return (2 * (Convert.ToDouble(widht) + (Convert.ToDouble(thickness)) * 2) + 2 * (Convert.ToDouble(height) + (Convert.ToDouble(thickness)) * 2));
+        }
+        public static double CalculateWindowPaneArea(string widht, string height, string thickness)
+        {
+            return (((Convert.ToDouble(widht) + (Convert.ToDouble(thickness) * 2)) * (Convert.ToDouble(height) + (Convert.ToDouble(thickness) * 2)))- (Convert.ToDouble(widht) * Convert.ToDouble(height)));
         }
     }
 }
